e9fc016552fe7b7db9e9aa7efdb82048c01fbc18
Ajewole Adedamola Jude <43986061+ajewolejude@users.noreply.github.com>
2019-05-28 15:56:06 +0100

    Organizing Code

---
+++ lib/app_screens/first_screen.dart
@@ -0,0 +1,28 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Material(
+      color: Colors.lightBlueAccent,
+      child: Center(
+          child:Text(
+              generateLuckyNumber(),
+            textDirection:TextDirection.ltr,
+            style: TextStyle(color: Colors.white, fontSize: 40.0 ),
+
+          )
+      ) ,
+    );
+  }
+
+
+  String generateLuckyNumber(){
+    var random = Random();
+    int luckcyNumber =  random.nextInt(10);
+    return "Your lucky number is $luckcyNumber";
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,27 @@
 import 'package:flutter/material.dart';
 
+import 'app_screens/first_screen.dart';
 
-void main(){
 
-  runApp(
+void main()=> runApp( new MyFlutterApp());
 
-    new MaterialApp(
+
+
+class MyFlutterApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "My Flutter App",
       home: Scaffold(
         appBar: AppBar(title: Text("Home"),),
-        body: Material(
-          color: Colors.lightBlueAccent,
-          child: Center(
-              child:Text(
-                "Hello Flutter",
-                textDirection:TextDirection.ltr,
-                style: TextStyle(color: Colors.white, fontSize: 40.0 ),
-
-              )
-          ) ,
-        )
+        body: FirstScreen()
         ,
       ),
-    ),
-      )
+    )
     ;
+  }
+
 
 }
