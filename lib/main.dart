dfb7c485acd012842d32cf1cdbf24c624d63b744
Ajewole Adedamola Jude <43986061+ajewolejude@users.noreply.github.com>
2019-05-28 15:10:17 +0100

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+
+void main(){
+
+  runApp(
+
+    new MaterialApp(
+      title: "My Flutter App",
+      home: Scaffold(
+        appBar: AppBar(title: Text("Home"),),
+        body: Material(
+          color: Colors.lightBlueAccent,
+          child: Center(
+              child:Text(
+                "Hello Flutter",
+                textDirection:TextDirection.ltr,
+                style: TextStyle(color: Colors.white, fontSize: 40.0 ),
+
+              )
+          ) ,
+        )
+        ,
+      ),
+    ),
+      )
+    ;
+
+}
