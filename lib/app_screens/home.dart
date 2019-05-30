08f0c099807470f7a9e46ab618d3530dde5ea8b4
Ajewole Adedamola Jude <43986061+ajewolejude@users.noreply.github.com>
2019-05-30 14:06:30 +0100

    Button & Alert Dialog

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -1,80 +1,79 @@
 import 'package:flutter/material.dart';
 
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(
-      child:
+        child:
         Container(
-          alignment: Alignment.center,
-          color: Colors.deepPurple,
-          padding: EdgeInsets.only(left:10.0, top: 40.0),
-          child:
-              Column(children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Expanded(child: Text(
-                      "Space Jet",
-                      textDirection: TextDirection.ltr,
-                      style: TextStyle(
-                        decoration: TextDecoration.none,
-                        fontSize: 35.0,
-                        fontFamily: 'Raleway',
-                        fontWeight: FontWeight.w700,
-                        color: Colors.white,
-                      ),
-                    )
-                      ,),
-                    Expanded( child: Text(
-                      "From Mumbai to Delhi",
-                      textDirection: TextDirection.ltr,
-                      style: TextStyle(
-                        decoration: TextDecoration.none,
-                        fontSize: 20.0,
-                        fontFamily: 'Raleway',
-                        fontWeight: FontWeight.w700,
-                        color: Colors.white,
-                      ),
+            alignment: Alignment.center,
+            color: Colors.deepPurple,
+            padding: EdgeInsets.only(left: 10.0, top: 40.0),
+            child:
+            Column(children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(child: Text(
+                    "Space Jet",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 35.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
                     ),
-                    )
-                  ],
-                ),
-
-                Row(
-                  children: <Widget>[
-                    Expanded(child: Text(
-                      "Space Ship",
-                      textDirection: TextDirection.ltr,
-                      style: TextStyle(
-                        decoration: TextDecoration.none,
-                        fontSize: 35.0,
-                        fontFamily: 'Raleway',
-                        fontWeight: FontWeight.w700,
-                        color: Colors.white,
-                      ),
-                    )
-                      ,),
-                    Expanded( child: Text(
-                      "From Kali to Lumba",
-                      textDirection: TextDirection.ltr,
-                      style: TextStyle(
-                        decoration: TextDecoration.none,
-                        fontSize: 20.0,
-                        fontFamily: 'Raleway',
-                        fontWeight: FontWeight.w700,
-                        color: Colors.white,
-                      ),
+                  )
+                    ,),
+                  Expanded(child: Text(
+                    "From London to Lagos",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 20.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
                     ),
-                    )
-                  ],
-                ),
+                  ),
+                  )
+                ],
+              ),
 
-                FlightImageAsset()
-              ],)
+              Row(
+                children: <Widget>[
+                  Expanded(child: Text(
+                    "Space Ship",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 35.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
+                    ),
+                  )
+                    ,),
+                  Expanded(child: Text(
+                    "From Abuja to London",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 20.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
+                    ),
+                  ),
+                  )
+                ],
+              ),
 
-
+              FlightImageAsset(),
+              FlightBookButton()
+            ],)
 
 
         )
@@ -82,14 +81,58 @@ class Home extends StatelessWidget{
   }
 
 
-
 }
 
-class FlightImageAsset extends StatelessWidget{
+class FlightBookButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    AssetImage assetImage = AssetImage('images/flight.jpg');
+    return Container(
+      margin: EdgeInsets.only(top: 30.0),
+      width: 250.0,
+      height: 50.0,
+      child: RaisedButton(
+
+          child: Text(
+            "Book Your Flight",
+            style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+                fontFamily: 'Raleway',
+                fontWeight: FontWeight.w700
+
+            ),
+          ),
+          color: Colors.deepOrange,
+          elevation: 6.0,
+
+          onPressed: () => bookFlight(context)
+      ),
+    );
+  }
+
+}
+
+void bookFlight(BuildContext context) {
+  var alertDialog = AlertDialog(
+    title: Text("Flight Booked Successfully"),
+    content: Text("Have a pleasant flight"),
+
+  );
+
+  showDialog(
+      context: context,
+      builder: (BuildContext context) => alertDialog
+
+  );
+}
+
+
+class FlightImageAsset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    AssetImage assetImage = AssetImage('images/flight.png');
     Image image = Image(image: assetImage);
 
     return Container(child: image,);
