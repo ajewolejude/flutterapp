1893a999e9bd0c921ba0d1d3e4e7f96a0e453c9e
Ajewole Adedamola Jude <43986061+ajewolejude@users.noreply.github.com>
2019-05-30 14:16:09 +0100

    Using List View

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,45 @@ void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: "ExploringUI widgets",
-    home: Home(),
+    home: Scaffold(
+      appBar: AppBar(title: Text("Basic List View"),),
+      body: getListView(),
+    )
 
   ));
+}
+
+
+Widget getListView(){
+
+  var listView = ListView(
+    children: <Widget>[
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text("Landscape"),
+        subtitle:Text( "Beautiful View"),
+        trailing: Icon(Icons.wb_sunny),
+        onTap: (){
+          debugPrint("Landscape tapped");
+        },
+        
+      ),
+
+      ListTile(
+        leading: Icon(Icons.laptop_chromebook),
+        title: Text("Windows"),
+
+
+      ),
+
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Phone"),
+
+
+      )
+    ],
+
+  );
+  return  listView;
 }
